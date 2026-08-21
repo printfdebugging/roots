25c53889498a7b269afdbcf05de45c6022f84696
Sahil Gautam <printfdebugging@gmail.com>
2026-08-21 21:31:20 +0530

    use fixed sized types backing the u8/u16.. types
    Before this, we were using int_least8_t for u8 and similar other types
    for the u16/u32.. type aliases. The problem with these types is that
    they are not gaurenteed to be the size the number says. So on some
    system, a u8 might be 32 bits.. and that would cause all sorts of
    issues.
    
    Signed-off-by: Sahil Gautam <printfdebugging@gmail.com>

--- source/editor.h
+++ source/editor.h
@@ -58,15 +58,15 @@
  * user defined convenience types *
  *********************************/
 
-typedef uint_least8_t u8;
-typedef uint_least16_t u16;
-typedef uint_least32_t u32;
-typedef uint_least64_t u64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
-typedef int_least8_t i8;
-typedef int_least16_t i16;
-typedef int_least32_t i32;
-typedef int_least64_t i64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
 
 typedef float f32;
 typedef double f64;
