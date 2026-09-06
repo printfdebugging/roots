56a2c88e12ac5fea5636884ca974d18ef4929076
Sahil Gautam <printfdebugging@gmail.com>
2026-09-07 03:25:33 +0530

    zero the static editor object, otherwise windows complaints in realloc
    We pass editor-><foo> pointers to realloc to expand the dynamic arrays
    as we add more objects. On windows, it fails because these pointers
    contained garbage values and trying to realloc those is a dangerous
    thing.
    
    Signed-off-by: Sahil Gautam <printfdebugging@gmail.com>

--- source/main.c
+++ source/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
    (void) argc;
    (void) argv;
 
-   struct Editor editor;
+   struct Editor editor = { 0 };
    if (!editorInit(&editor) || !editorRun(&editor) || !editorDeInit(&editor))
       return EXIT_FAILURE;
    return EXIT_SUCCESS;
