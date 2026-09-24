b1738f5396de97266c0be79cb8e263a2264689ce
Sahil Gautam <printfdebugging@gmail.com>
2026-09-25 03:24:16 +0530

    add some spaces around text api function decls
    Signed-off-by: Sahil Gautam <printfdebugging@gmail.com>

--- include/text.h
+++ include/text.h
@@ -9,10 +9,15 @@
 struct Text;
 
 struct Text *TextLoadFromFile(const char *filepath);
+
 struct Text *TextLoadFromData(const char *data, u32 dataLength);
+
 bool TextWriteToFile(const char *filepath);
+
 u32 TextGetLineCount(struct Text *text);
+
 char *TextGetUTF8Line(struct Text *text, u32 line);
+
 void TextDestroy(struct Text *text);
 
 u32 TextGetLineLength(struct Text *text, u32 lineIdx);
